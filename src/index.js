cbc5c9964d4b52df8de9feed003885e99a8e4bad
Ilina <anastasia.ilina@t-systems.ru>
2020-10-06 12:16:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const array = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (const elements of str.split``) {
+    if (array[0] === elements) {
+        array.shift();
+      } else if (brackets[elements]) {
+        array.unshift(brackets[elements]);
+      } else {
+          return false;
+      }
+    }
+
+  return array.length === 0;
+
 }
